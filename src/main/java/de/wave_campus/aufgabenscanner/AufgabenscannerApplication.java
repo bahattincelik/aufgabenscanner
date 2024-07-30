92d3c94a27d3ed194b9f1df331f7a5f81fd9b787
Bahattin Celik <celik@wave-solutions.de>
2024-07-30 13:03:52 +0200

    Initial commit

---
+++ src/main/java/de/wave_campus/aufgabenscanner/AufgabenscannerApplication.java
@@ -0,0 +1,13 @@
+package de.wave_campus.aufgabenscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AufgabenscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AufgabenscannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wave_campus/aufgabenscanner/AufgabenscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wave_campus.aufgabenscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AufgabenscannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
